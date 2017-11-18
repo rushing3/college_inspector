4163f6f572d71ed36cb5c5a7550af49f3aa8f5bd
Alex Rushing <arushing95@gmail.com>
2017-11-18 16:59:05 -0500

    Add some stuff to get started

--- js/main.js
+++ js/main.js
@@ -5,15 +5,37 @@ var svgWidth = +svg.attr('width');
 var svgHeight = +svg.attr('height');
 
 // Define a padding object
-var padding = {t: 20, r: 20, b: 20, l: 250};
+var padding = {t: 20, r: 20, b: 20, l: 20};
 
-function Visual(x, y) {
+function Visual(x) {
     this.x = x;
-    this.y = y;
 }
 
-Visual.prototype.init = function(g, data) {
-    var viz = d3.select(g);
+Visual.prototype.init = function(data, group) {
+    var viz = d3.select(group);
+
+    viz.append('text')
+        .text(function(d) {
+            return d.key;
+        });
+    viz.append('text')
+        .text(function(d) {
+            return '# of Colleges: ' + d.values.length;
+        })
+        .attr('transform', 'translate(10, 25)');
+
+    viz.selectAll('.college')
+        .data(data.values)
+        .enter()
+        .append('circle')
+        .attr('r', 3)
+        .attr('cx', function(d, i) {
+            return 0;
+        })
+        .attr('cy', function(d, i) {
+            return (i+1)*7;
+        });
+
 }
 
 d3.csv('./data/colleges.csv',
@@ -36,15 +58,27 @@ function(error, dataset){
         return;
     }
 
+    var regionData = d3.nest()
+        .key(function(d) {
+            return d.region;
+        })
+        .entries(dataset);
+
     // **** Your JavaScript code goes here ****
-    var histoG = svg.selectAll('.viz')
-        .data(dataset)
+    var regionViz = svg.selectAll('.viz')
+        .data(regionData)
         .enter()
         .append('g')
         .attr('transform', function(d,i) {
-            var tx = (i * 2);
+            var tx = (i * ((svgWidth - padding.l - padding.r)/regionData.length)) + padding.l;
             var ty = (padding.t);
             return 'translate('+[tx, ty]+')';
         });
+    console.log(regionData);
+
+    regionViz.each(function(d) {
+        rV = new Visual(0);
+        rV.init(d, this);
+    });
 
 });
