7b30b660ec8ac81628a8b14a8cdc211ce49cb40d
Alex Rushing <arushing95@gmail.com>
2017-11-18 22:05:51 -0500

    Color code dots by locale

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ var svgHeight = +svg.attr('height');
 // Define a padding object
 var padding = {t: 20, r: 20, b: 20, l: 20};
 
+// Locale color scale
+var localeColorScale = d3.scaleOrdinal(d3.schemeDark2);
+
 function Visual(x) {
     this.x = x;
 }
@@ -14,26 +17,38 @@ function Visual(x) {
 Visual.prototype.init = function(data, group) {
     var viz = d3.select(group);
 
+    // Sort data by locale
+    data.values.sort(function(a, b) {
+        return d3.descending(a.locale, b.locale);
+    });
+
+    // Add region label
     viz.append('text')
         .text(function(d) {
             return d.key;
         });
+
+    // Add label for how many colleges are in that region
     viz.append('text')
         .text(function(d) {
             return '# of Colleges: ' + d.values.length;
         })
         .attr('transform', 'translate(10, 25)');
 
+    // Add dot for each college in region, color coded by locale
     viz.selectAll('.college')
         .data(data.values)
         .enter()
         .append('circle')
-        .attr('r', 3)
+        .attr('fill', function(d) {
+            return localeColorScale(d.locale);
+        })
+        .attr('r', 5)
         .attr('cx', function(d, i) {
-            return (i%15)*7 + 10;
+            return (i%11)*11 + 10;
         })
         .attr('cy', function(d, i) {
-            return Math.floor(i/15)*7 + 40;
+            return Math.floor(i/11)*11 + 40;
         });
 
 }
@@ -58,13 +73,34 @@ function(error, dataset){
         return;
     }
 
+    // Nest locale data
+    var localeData = d3.nest()
+        .key(function(d) {
+            return d.locale;
+        })
+        .entries(dataset);
+
+    // Nest region data
     var regionData = d3.nest()
         .key(function(d) {
             return d.region;
         })
         .entries(dataset);
 
-    // **** Your JavaScript code goes here ****
+    // Set localColor domain to be set of all unique locales
+    localeColorScale.domain(localeData.map(function(d) {
+        return d.key;
+    }));
+
+    // Define legend for locale colors
+    var localeLegend = d3.legendColor().scale(localeColorScale);
+
+    // Add legend for locale colors
+    svg.append('g')
+        .attr('transform', 'translate(20, 500) scale(0.8, 0.8)')
+        .call(localeLegend);
+
+    // Add a group for each region
     var regionViz = svg.selectAll('.viz')
         .data(regionData)
         .enter()
@@ -74,8 +110,8 @@ function(error, dataset){
             var ty = (padding.t);
             return 'translate('+[tx, ty]+')';
         });
-    console.log(regionData);
 
+    // Make and add Viz for each region group
     regionViz.each(function(d) {
         rV = new Visual(0);
         rV.init(d, this);
